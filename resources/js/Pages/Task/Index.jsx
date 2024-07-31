b6cfc8a2af6105aebfc36a36ef30b1d4b5b2b012
Davoliva <david.arturo.oliva.bahamondez@gmail.com>
2024-07-31 11:12:41 -0400

    la tabla con tareas se convierte en un componente para reutilizarlo

--- resources/js/Pages/Task/Index.jsx
+++ resources/js/Pages/Task/Index.jsx
@@ -7,7 +7,7 @@ export default function index({ auth, tasks, queryParams = null }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Proyectos</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Tareas</h2>}
         >
             <Head title="Tasks" />
 
